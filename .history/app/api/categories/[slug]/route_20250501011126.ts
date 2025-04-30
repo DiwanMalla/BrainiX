3c4d42b72d3da347af4f2d951c289f05d350b5c3
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2025-05-01 01:16:25 +1000

    .

--- .history/app/api/categories/[slug]/route_20250501011126.ts
+++ .history/app/api/categories/[slug]/route_20250501011126.ts
@@ -2,6 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/lib/db";
 
 export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
   const slug = request.nextUrl.pathname.split("/").pop(); // Extract slug from the URL path
 
   try {
